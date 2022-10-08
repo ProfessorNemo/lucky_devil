1033780a60a00390d00441ed50e751c352278915
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-10-08 23:57:26 +0300

    create factories

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,14 @@
 
 FactoryBot.define do
   factory :question do
+    # Ответы сделаем рандомными для красоты
+    answer1 { rand(2001).to_s }
+    answer2 { rand(2001).to_s }
+    answer3 { rand(2001).to_s }
+    answer4 { rand(2001).to_s }
+
+    sequence(:text) { |n| "В каком году была космическая одиссея #{n}?" }
+
+    sequence(:level) { |n| n % 15 }
   end
 end
